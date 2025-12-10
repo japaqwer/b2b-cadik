a2b2fdc805273e2ab949151011a1f374388bfe80
japaqwer <aktanjapa@gmail.com>
2025-12-10 23:18:03 +0600

    sdfgs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.scss";
 import FAQ from "@/components/layout/FAQ/FAQ";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -12,6 +13,19 @@ export default function RootLayout({ children }) {
         <meta name="robots" content="noindex,nofollow" />
       </head>
       <body>
+        <Script
+          type="text/javascript"
+          id="yandex-metrika"
+          strategy="afterInteractive"
+        >
+          {` (function(m,e,t,r,i,k,a){
+        m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105768630', 'ym');
+    ym(105768630, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
         <Header />
         <main className="container">{children}</main>
         <FAQ />
