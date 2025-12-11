a7d1bf3ce7ac8a4092f9ade09f4f8394db38da80
japaqwer <aktanjapa@gmail.com>
2025-12-11 17:03:36 +0600

    dfgsdf

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,14 +18,32 @@ export default function RootLayout({ children }) {
           id="yandex-metrika"
           strategy="afterInteractive"
         >
-          {` (function(m,e,t,r,i,k,a){
-        m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105768630', 'ym');
-    ym(105768630, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+          {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+          
+          ym(105768630, 'init', {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true,
+            ecommerce:"dataLayer"
+          });`}
         </Script>
+        
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/105768630" 
+              style={{position: "absolute", left: "-9999px"}} 
+              alt="" 
+            />
+          </div>
+        </noscript>
+
         <Header />
         <main className="container">{children}</main>
         <FAQ />
